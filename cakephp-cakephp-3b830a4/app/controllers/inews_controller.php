44e67c02062f6880614b356f910042dc63d9b685
Hyun Seumg Bum <orion203@gmail.com>
2011-11-07 18:27:02 +0900

    inews controller modified...

--- cakephp-cakephp-3b830a4/app/controllers/inews_controller.php
+++ cakephp-cakephp-3b830a4/app/controllers/inews_controller.php
@@ -6,12 +6,17 @@ class InewsController extends AppController {
 	var $helpers = array('Text');
 
 	function index() {
-		$this->set('cateTitle', 'IT News'); 
 		App::import('Component','Simplepie');     // load it
 		if(!isset($this->Simplepie)) {
 			$this->Simplepie = new SimplepieComponent();
 			$this->Simplepie->startup($this);
 			$items = $this->Simplepie->feed('http://feeds.feedburner.com/TheGeekStuff');
+			$site = $this->Simplepie->get_site();
+			$siteLink = $this->Simplepie->get_siteLink();
+			$siteDescription = $this->Simplepie->get_siteDescription();
+			$this->set('site',$site);
+			$this->set('siteLink', $siteLink);
+			$this->set('siteDescription',$siteDescription);
 			$this->set('feeds', $items);
 		}
 	}
